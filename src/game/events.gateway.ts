8e4f520eaf6005ade4648302c93359ec72b2d8e8
Ari Stoler <astoler428@gmail.com>
2024-03-31 12:36:11 -0500

    merged staging

--- src/game/events.gateway.ts
+++ src/game/events.gateway.ts
@@ -37,16 +37,11 @@ class ChatMessageDTO {
 
 @WebSocketGateway({
   cors: true,
-<<<<<<< HEAD
   origin: [
     'https://blind-sh.netlify.app',
     'https://blind-sh-staging.netlify.app',
   ],
   // origin: 'http://localhost:3001',
-=======
-  // origin: 'https://blind-sh.netlify.app',
-  origin: 'http://localhost:3001',
->>>>>>> ari/dev
   transports: ['websocket', 'polling'],
 })
 export class EventsGateway {
