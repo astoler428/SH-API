23b12e24c2e28367a4b92a8d693b748dc07b6f66
Ari Stoler <astoler428@gmail.com>
2024-04-13 09:32:03 -0500

    changed date format

--- src/helperFunctions.ts
+++ src/helperFunctions.ts
@@ -2,8 +2,9 @@ import { GameType, Status } from './consts';
 import { Game } from './models/game.model';
 
 export function getFormattedDate() {
-  const addLeadingZero = (val: number) => (val < 10 ? `0${val}` : `${val}`);
   const now = new Date();
+  return now.toISOString();
+  const addLeadingZero = (val: number) => (val < 10 ? `0${val}` : `${val}`);
   const hours = addLeadingZero(now.getHours());
   const minutes = addLeadingZero(now.getMinutes());
   const seconds = addLeadingZero(now.getSeconds());
